5700bb48c9332ea0a7e11dc10418fe2dae69703f
bes01 <bkapa18@freeuni.edu.ge>
2022-10-03 20:03:30 +0400

    Initial commit

---
+++ src/main/java/ge/bkapa/discsovery/assigment/DiscsoveryAssigmentApplication.java
@@ -0,0 +1,13 @@
+package ge.bkapa.discsovery.assigment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscsoveryAssigmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscsoveryAssigmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ge/bkapa/discsovery/assigment/DiscsoveryAssigmentApplicationTests.java
@@ -0,0 +1,13 @@
+package ge.bkapa.discsovery.assigment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscsoveryAssigmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
