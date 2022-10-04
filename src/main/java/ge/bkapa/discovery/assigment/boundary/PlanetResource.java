27bfb004f580d0d8cd8f00e0979370bfdabc6507
bes01 <bkapa18@freeuni.edu.ge>
2022-10-04 21:37:26 +0400

    fixed request mapping bug

--- src/main/java/ge/bkapa/discovery/assigment/boundary/PlanetResource.java
+++ src/main/java/ge/bkapa/discovery/assigment/boundary/PlanetResource.java
@@ -7,12 +7,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.util.List;
 
-@RestController("planets")
+@RestController
+@RequestMapping("planets")
 public class PlanetResource {
 
     private final PlanetService service;
--- src/main/java/ge/bkapa/discovery/assigment/boundary/RoutingResource.java
+++ src/main/java/ge/bkapa/discovery/assigment/boundary/RoutingResource.java
@@ -3,10 +3,12 @@ package ge.bkapa.discovery.assigment.boundary;
 import ge.bkapa.discovery.assigment.algorithm.model.Path;
 import ge.bkapa.discovery.assigment.service.RoutingService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("routing")
+@RestController
+@RequestMapping("routing")
 public class RoutingResource {
 
     private final RoutingService service;
